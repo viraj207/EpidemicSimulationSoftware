f6eab8ab5558ddae26b6b14d5ef8928a0d6c684a
charlesreillyUOL <78875110+charlesreillyUOL@users.noreply.github.com>
2022-04-23 19:15:08 +0100

    Update Person.java

--- Person.java
+++ Person.java
@@ -7,10 +7,10 @@ public class Person {
 	Color P_BLUE = new Color(160,196,255);
 	Color P_RED = new Color(255,173,173);
 	Color P_ORANGE= new Color (255, 214, 165);
-	int x, y;
-	int xVelocity, yVelocity;
-	int age;
-	int infectionStatus = 0;
+	int x, y; // Integers representing the X AND Y position of this Person object.
+	int xVelocity, yVelocity; // Integers representing the magnitude of X AND Y movement. Allowing us to determine direction aswell. 
+	int age; 
+	int infectionStatus = 0; // 0 -> healthy, 1 -> infected, 2 -> recovered and 3 -> deceased.
 	boolean isVaccinated = true , isMasked = true;
 	double deathChance, hygieneLevel, contagiousness;
 	int healingTime;
@@ -19,13 +19,18 @@ public class Person {
 	public Person(int pLevel,double iLevel, double mLevel, double hLevel, double cLevel, 
 				  double lLevel, double sLevel, double vLevel, int tLevel) {
 		
-		numTotal = pLevel;
-		deathChance = lLevel;
-		hygieneLevel = hLevel;
-		contagiousness = cLevel;
+		numTotal = pLevel; // Assigning of the population size. 
+		deathChance = lLevel; // Assigning of the lethality. 
+		hygieneLevel = hLevel; // Assigning of hygiene levels.
+		contagiousness = cLevel; // Assigning of contagiousness.
+		
+		/* Randomises the position of referenced 
+		Person object to within the screen limits */
 		x = (int)(Math.random()*690+0);
 		y = (int)(Math.random()*480+0);
-		age = (int)(Math.random()*(84+1)+16);
+		
+		
+		age = (int)(Math.random()*(84+1)+16); // Generates a random age for the Person between 16 and 85
 		if(Math.random()<0.01*vLevel) {
 			isVaccinated = false;
 			deathChance *= 0.05;
@@ -45,7 +50,7 @@ public class Person {
 			yVelocity  = (int)(Math.random()*(6+1)+-3);
 		}
 		
-		healingTime = (int)(Math.random()*(14000-3000+1)+3000)+(age*100);
+		healingTime = (int)(Math.random()*(14000-3000+1)+3000)+(age*100); // Time of recovery is determined by randomness aswell as, age.
 		
 	}
 	
